27e50b3bf628aae7ae71b65dab9bb9c97db69760
dongju3 <dongju8679@gmail.com>
2019-01-02 00:42:53 +0900

    using malloc

--- hash_t3.cpp
+++ hash_t3.cpp
@@ -4,11 +4,21 @@
 
 using namespace std;
 
+#define MAX_TABLE 10
+#define MAX_N 10
+#define MAX_LEN 15
+
+typedef struct dat_t{
+    char key[15];
+    int val;
+}dat_t;
+
+int N;
+
 unsigned long myhash(const char *str)
 {
     unsigned long hash = 5381;
     int c;
-
     while(c = *str++)
     {
         hash = (((hash << 5) + hash) + c) % MAX_TABLE;
@@ -19,9 +29,25 @@ unsigned long myhash(const char *str)
 int main(void)
 {
     int ret = 0;
-    printf("dongju start2\n");
-    freopen("hash_t2.txt", "r", stdin);
+    int i;
+    freopen("hash_t3.txt", "r", stdin);
     scanf("%d", &N);
     printf("%d\n", N);
+    
+    dat_t **ptr = NULL;
+    ptr = (dat_t **)malloc(sizeof(dat_t *) * N);
+    for (i = 0; i < N; i++) {
+
+        *(ptr + i) = (dat_t *)malloc(sizeof(dat_t) * MAX_LEN);
+        scanf( "%s", &(*(ptr + i))->key[0] );
+        scanf( "%d", &(*(ptr + i))->val );
+        printf("input %s, val(%d)\n", (*(ptr + i))->key, (*(ptr + i))->val );
+    }
+
+    for (i = 0; i < N; i++) {
+        free(*(ptr+i));
+    }
+    free(ptr);
+
     return ret;
 }
